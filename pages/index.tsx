11d563b79e74a6576053ce09dfadccc9bfcc3f25
Ti4JoaoMatheus <99748402+Ti4JoaoMatheus@users.noreply.github.com>
2023-12-21 11:23:53 -0300

    ajuste

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Theme } from '@mui/system'
+import Layout from '../componentes/Layout'
+
+const Home = () => {
+
+  return (
+    <>
+      <Layout>
+            
+      </Layout>  
+    </>    
+  )
+}
+
+export default Home
